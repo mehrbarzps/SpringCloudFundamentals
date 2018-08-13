251b8026a797cd8eefcdf574aadcc9823152dc7a
mehrbarz <mehrbarzegar@gmail.com>
2018-08-13 14:03:16 +0200

    third

--- client/src/main/java/io/mehr/barz/client/ClientApplication.java
+++ client/src/main/java/io/mehr/barz/client/ClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -20,11 +20,10 @@ import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
 public class ClientApplication {
 	
 	@Autowired
-	private EurekaClient client; 
+	private EurekaClient client;
 	
 	@Autowired
 	private RestTemplateBuilder restTemplateBuilder;
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
@@ -33,11 +32,11 @@ public class ClientApplication {
 	@RequestMapping("/")
 	public String callService() {
 		RestTemplate restTemplate = restTemplateBuilder.build();
-		
 		InstanceInfo instanceInfo = client.getNextServerFromEureka("service", false);
 		String baseUrl = instanceInfo.getHomePageUrl();
-		ResponseEntity<String> response = 
+		ResponseEntity<String> response =
 				restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
 		return response.getBody();
 	}
 }
+
